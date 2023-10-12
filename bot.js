940537373a1b9f9eed49ca7bbc4465ae4062af29
AqilJaafree <wanaqilre@gmail.com>
2023-10-13 01:42:57 +0800

    ai botchat

--- bot.js
+++ bot.js
@@ -1,28 +1,37 @@
-const  Telegraf  = require("telegraf");
-// Import the axios module
-const axios = require("axios"); 
+const Telegraf = require("telegraf");
+const axios = require("axios");
 
 const bot = new Telegraf("5952399566:AAGjjMx7HdC4CSGPALCeHnUSzj8RQBAOAXQ");
-bot.mention("hangNFT_bot", (ctx) => {
-  ctx.reply("Hello" + ctx.from.first_name);
 
-  // Making the HTTP request to the provided API endpoint
-  axios.post(
+bot.mention("hangNFT_bot", async (ctx) => {
+  try {
+    // Get the user's message
+    const userMessage = ctx.message.text;
+    const username = ctx.message.from.username; 
+    const response = await axios.post(
       "https://khalil-himura-gmail-com-280-rasa.nous.mesolitica.com/webhooks/rest/webhook",
       {
         sender: "test_user",
-        message: "Hi there!",
+        message: userMessage,
       }
-    )
-    .then((response) => {
-      console.log("API Response:", response.data);
-      // Handle the API response here
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-      // Handle errors here
-    });
-});
+    );
 
+    let allText = "";
+
+    for (const d of response.data) {
+      if (d.text) {
+        allText += `${d.text}`;
+      }
+    }
+
+        ctx.reply(`@${username} ${allText || "No response from the API."}`);
+
+  } catch (error) {
+    let allText = "";
+    ctx.reply(
+      allText || "Error occurred, but no additional information available."
+    );
+  }
+});
 
 bot.launch();
